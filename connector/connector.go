c7ea1295179fa55d4d58655dafc5597661241dbf
Denis Denysyev <dvdenysyev@gmail.com>
2022-09-20 15:15:32 +0200

    Refactor code

--- connector/connector.go
+++ connector/connector.go
@@ -24,7 +24,7 @@ func NewConnector(serviceUrl string) ConnectorI {
 		},
 	}
 }
-func (c Connector) DoAuthRequest(method string, url string, token string, body interface{}) (*http.Response, error) {
+func (c Connector) DoAuthRequest(method string, url string, token string, body interface{}, expectedStatus int) (*http.Response, error) {
 	postBody, err := json.Marshal(body)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to marshal")
@@ -41,10 +41,14 @@ func (c Connector) DoAuthRequest(method string, url string, token string, body i
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to send request")
 	}
+	if expectedStatus != response.StatusCode {
+		response.Body.Close()
+		return nil, errors.New("Bad status")
+	}
 	return response, nil
 }
-func (c Connector) DoAuthRequestWithDecode(method string, url string, token string, body interface{}, decodeModel interface{}) error {
-	response, err := c.DoAuthRequest(method, url, token, body)
+func (c Connector) DoAuthRequestWithDecode(method string, url string, token string, body interface{}, decodeModel interface{}, expectedStatus int) error {
+	response, err := c.DoAuthRequest(method, url, token, body, expectedStatus)
 	if err != nil {
 		return err
 	}
@@ -59,24 +63,32 @@ func (c Connector) DoAuthRequestWithDecode(method string, url string, token stri
 
 func (c Connector) GenerateJwtPair(address string, purpose string) (resources.JwtPairResponse, error) {
 	model := &resources.JwtPairResponse{}
-	return *model, c.DoAuthRequestWithDecode("POST", c.ServiceUrl+"/get_token_pair", "", NewClaimsModel(address, purpose), model)
+	return *model, c.DoAuthRequestWithDecode("POST", c.ServiceUrl+"/get_token_pair", "", NewClaimsModel(address, purpose), model, http.StatusOK)
 }
 
 func (c Connector) ValidateJwt(token string) (string, error) {
 	model := &resources.JwtValidationResponse{}
-	return *&model.Data.Attributes.EthAddress, c.DoAuthRequestWithDecode("POST", c.ServiceUrl+"/validate_token", token, nil, model)
+	err := c.DoAuthRequestWithDecode("POST", c.ServiceUrl+"/validate_token", token, nil, model, http.StatusOK)
+	if err != nil {
+		return "", err
+	}
+	if (*model).Data.Attributes.EthAddress == "" {
+		return "", errors.New("failed to validate")
+	}
+	return (*model).Data.Attributes.EthAddress, nil
 }
 
 func (c Connector) RefreshJwt(refreshToken string) (resources.JwtPairResponse, error) {
 	model := &resources.JwtPairResponse{}
-	return *model, c.DoAuthRequestWithDecode("POST", c.ServiceUrl+"/refresh_token", refreshToken, nil, model)
+
+	return *model, c.DoAuthRequestWithDecode("POST", c.ServiceUrl+"/refresh_token", refreshToken, nil, model, http.StatusOK)
 }
 func (c Connector) GetAuthToken(r *http.Request) (string, error) {
 	return helpers.Authenticate(r)
 }
 
 func (c Connector) CheckPermission(owner string, token string) (bool, error) {
-	response, err := c.DoAuthRequest("POST", c.ServiceUrl+"/check_permission", token, NewCheckPermissionModel(owner))
+	response, err := c.DoAuthRequest("POST", c.ServiceUrl+"/check_permission", token, NewCheckPermissionModel(owner), http.StatusNoContent)
 	defer response.Body.Close()
 	return response.StatusCode == http.StatusNoContent, err
 }
