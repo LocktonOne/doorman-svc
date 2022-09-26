5483c8b52faecc5bc92f0e3bf81b2bd223d318db
Denis Denysyev <dvdenysyev@gmail.com>
2022-09-26 16:44:04 +0200

    Fix

--- internal/service/requests/check_resource_permission.go
+++ internal/service/requests/check_resource_permission.go
@@ -3,14 +3,13 @@ package requests
 import (
 	"net/http"
 
-	"github.com/go-chi/chi"
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/pkg/errors"
 	"gitlab.com/tokene/doorman/internal/service/helpers"
 )
 
 func NewCheckResourcePermission(r *http.Request) (string, error) {
-	owner := chi.URLParam(r, "owner")
+	owner := r.URL.Query().Get("owner")
 	if err := validation.Validate(owner, validation.Required, validation.Match(helpers.AddressRegexp)); err != nil {
 		return owner, errors.Wrap(err, "failed to get parameter")
 	}
